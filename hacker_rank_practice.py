9c4e160850f64d7fd32cf43c1e9a00dfb4e000e2
Muhammad Maaz <35654937+maazu@users.noreply.github.com>
2020-06-02 03:32:43 +0100

    Update hacker_rank_practice.py

--- hacker_rank_practice.py
+++ hacker_rank_practice.py
@@ -21,3 +21,13 @@ def is_leap(year):
         leap = True
    
     return leap
+
+'''
+Without using any string methods, try to print the following:
+123..N
+'''
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1,n+1):
+        print(i,end="")
+    
